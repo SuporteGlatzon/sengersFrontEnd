e76acecaadcc48ceadbe23e5b7040693a1751eda
Gustavo-Krauss <gustavomkrauss@gmail.com>
2025-09-24 18:51:20 -0300

    Banner-atualizado

--- src/components/banner/index.tsx
+++ src/components/banner/index.tsx
@@ -1,10 +1,10 @@
 'use client';
 import { BannerProps } from '@/app/type/banner.type';
+import Image from 'next/image';
 import 'swiper/css';
 import 'swiper/css/effect-fade';
 import { Autoplay, EffectFade, Navigation } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import Button from '../button';
 
 type Props = {
   banners: BannerProps[];
@@ -12,12 +12,12 @@ type Props = {
 
 export const Banner = ({ banners }: Props) => {
   return (
-    <div className='custom-container'>
+    <div className="custom-container">
       <Swiper
         modules={[Navigation, EffectFade, Autoplay]}
         slidesPerView={1}
         spaceBetween={30}
-        effect='fade'
+        effect="fade"
         loop={true}
         autoplay={{
           delay: 7000,
@@ -28,57 +28,25 @@ export const Banner = ({ banners }: Props) => {
           nextEl: '.next',
           disabledClass: 'swiper-button-disabled opacity-60',
         }}
-        breakpoints={{
-          0: {
-            slidesPerView: 1,
-            spaceBetween: 20,
-          },
-          768: {
-            slidesPerView: 1,
-            spaceBetween: 40,
-          },
-          1024: {
-            slidesPerView: 1,
-            spaceBetween: 50,
-          },
-        }}
       >
-        {banners.map((banner, index) => {
-          return (
-            <SwiperSlide key={index} className='py-4'>
-              <div className='relative rounded-2xl md:rounded-[42px] overflow-hidden'>
-                {/* <Image
-                  src={getVersionedImageUrl(banner.image)}
-                  width={1920}
-                  height={1080}
-                  alt={banner.title}
-                  className='aspect-[9/16] md:aspect-square lg:aspect-[1280/570] object-cover rounded-lg overflow-hidden'
-                /> */}
-                <div className='absolute z-50 bottom-0 left-0 h-full w-full text-white p-8 md:p-20 flex justify-end flex-col'>
-                  <div className='max-w-xl flex flex-col gap-5'>
-                    <h1 className='text-2xl lg:text-7xl font-bold'>
-                      {banner.title}
-                    </h1>
-                    <div className='text-small lg:text-base'>
-                      {banner.subtitle}
-                    </div>
-
-                    {banner.button_link && banner.button_text && (
-                      <Button
-                        href={banner.button_link}
-                        variant='tertiary'
-                        borderRadius='full'
-                        className='self-start'
-                      >
-                        {banner.button_text}
-                      </Button>
-                    )}
-                  </div>
-                </div>
+        {banners.map((banner, index) => (
+          <SwiperSlide key={index} className="py-4">
+            <div className="relative rounded-2xl md:rounded-[42px] overflow-hidden">
+              <Image
+                src={banner.image} // ✅ pega direto do backend
+                width={1920}
+                height={1080}
+                alt={banner.title}
+                className="aspect-[9/16] md:aspect-square lg:aspect-[1280/570] object-cover rounded-lg overflow-hidden"
+              />
+              <div className="absolute z-50 bottom-0 left-0 h-full w-full text-white p-8 md:p-20 flex justify-end flex-col bg-gradient-to-t from-black/60 to-transparent">
+                <h1 className="text-2xl lg:text-6xl font-bold">
+                  {banner.title}
+                </h1>
               </div>
-            </SwiperSlide>
-          );
-        })}
+            </div>
+          </SwiperSlide>
+        ))}
       </Swiper>
     </div>
   );
